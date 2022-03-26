d473668d49886e5edf47a65a73e7732d46b3653d
artjombokov <abokov@tktk.ee~>
2022-03-26 15:43:21 +0200

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomChuckNorrisJoke;
+            randomChuckNorrisJoke = GetRandomJokeFromFile("chuck.txt");
+
+            Console.WriteLine($"Here's a joke for you: {randomChuckNorrisJoke}.");
+
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string GetRandomJokeFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\samplestktk\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+
+    }
+}
